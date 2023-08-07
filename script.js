43b710573c06d0bb8c777c12cb5a3bb31e2193dc
Aytan-sudo <cantais.aymeric@gmail.com>
2023-08-07 19:30:37 +0200

    new file:   img/diamond_pink.png 	new file:   img/diamond_shine.png 	new file:   img/diamond_yellow.png 	modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -10,22 +10,54 @@ function random(){ // generate a number between 0 and 9
     return i;
 }
 
-var start_button = document.getElementById("start");
-start_button.addEventListener('click', function(){
+function randomize_numbers() {
     num_A = random();
     num_B = random();
     document.getElementById('NumA').innerHTML = num_A;
     document.getElementById('NumB').innerHTML = num_B;
-    document.getElementById('start').style.visibility = 'hidden';
+}
+
+function add_diamonds(x) {
+    var num_diamonds = document.getElementById("diamonds");
+    if (number_diamonds < 11) {
+        for (var i = 0; i < x; i++) {
+            num_diamonds.innerHTML += "<img src='img/diamond_yellow.png' height=64px/>";
+        }
+    }  else if (number_diamonds < 21) {
+        for (var i = 0; i < x; i++) {
+            if (number_diamonds == 11) {num_diamonds.innerHTML = ""};
+            num_diamonds.innerHTML += "<img src='img/diamond_pink.png' height=64px/>";
+        }
+    } else if (number_diamonds < 31) {
+        for (var i = 0; i < x; i++) {
+            if (number_diamonds == 21) {num_diamonds.innerHTML = ""};
+            num_diamonds.innerHTML += "<img src='img/diamond_shine.png' height=64px/>";
+        } 
+    } else {num_diamonds.innerHTML = "Victoire !"};
+}
+
+
+var start_button = document.getElementById("start");
+start_button.addEventListener('click', function(){
+    randomize_numbers();
+    document.getElementById('start_box').style.visibility = 'hidden';
     document.getElementById('question').style.visibility = 'visible';
+    document.getElementById('answer').style.visibility = 'visible';
 })
 
 var submitting_answer = document.getElementById("submitting_answer");
 submitting_answer.addEventListener('keydown', function (e) {
     if (e.key === "Enter") {
-        // check validation
-        number_diamonds += 1;
-        alert(number_diamonds);
+        var input = document.getElementById("submitting_answer").value;
+        if (num_A * num_B == input) {
+            number_diamonds += 1;
+            add_diamonds(1);
+        } else {
+            number_diamonds = 0;
+            document.getElementById("diamonds").innerHTML = "";
+        }
+        randomize_numbers();
+        submitting_answer.value = "";
     }
 })
 
