0b9a8ea8f339969c43efa6a3505604f7b467855a
Aytan-sudo <cantais.aymeric@gmail.com>
2023-08-08 23:27:49 +0200

    new file:   img/theme.mp3 new file:   img/victory.mp3 modified:   index.html modified:   script.js modified:   style.css

--- script.js
+++ script.js
@@ -1,22 +1,44 @@
+// initialize the parameters
 let number_diamonds = 0;
 let num_A = 0;
 let num_B = 0;
-document.getElementById('NumA').innerHTML = num_A;
-document.getElementById('NumB').innerHTML = num_B;
 
+// declare the audio
+const audio_theme = new Audio("img/theme.mp3");
+audio_theme.loop = true;
+audio_theme.volume=0.8;
+const audio_victory = new Audio("img/victory.mp3");
 
-function random(){ // generate a number between 0 and 9
-    i = Math.floor(Math.random() * 10);
-    return i;
-}
+// import code of confettis (victory celebration)
+function confettis_victory(){
+    confetti({
+        particleCount: 100,
+        spread: 360,
+        origin: {
+            x: Math.random(),
+            y: Math.random() - 0.2}
+    });
+    }
 
 function randomize_numbers() {
-    num_A = random();
-    num_B = random();
+    num_A = Math.floor(Math.random() * 10);
+    num_B = Math.floor(Math.random() * 10);
     document.getElementById('NumA').innerHTML = num_A;
     document.getElementById('NumB').innerHTML = num_B;
 }
 
+// initialization of the game
+var start_button = document.getElementById("start");
+start_button.addEventListener('click', function(){
+    randomize_numbers();
+    document.getElementById('start_box').style.visibility = 'hidden';
+    document.getElementById('question').style.visibility = 'visible';
+    document.getElementById('answer').style.visibility = 'visible';
+    document.getElementById('submitting_answer').focus()
+    audio_theme.play()
+})
+
+// diamonds are the score. 3 rows of 10 diamonds before victory
 function add_diamonds(x) {
     var num_diamonds = document.getElementById("diamonds");
     if (number_diamonds < 11) {
@@ -33,18 +55,17 @@ function add_diamonds(x) {
             if (number_diamonds == 21) {num_diamonds.innerHTML = ""};
             num_diamonds.innerHTML += "<img src='img/diamond_shine.png' height=64px/>";
         } 
-    } else {num_diamonds.innerHTML = "Victoire !"};
+    } else {
+        num_diamonds.innerHTML = "VICTOIRE !";
+        audio_theme.pause();
+        audio_victory.play();
+        document.getElementById('question').style.visibility = 'hidden';
+        document.getElementById('answer').style.visibility = 'hidden';
+        setInterval(confettis_victory, 750);        
+    }
 }
 
-
-var start_button = document.getElementById("start");
-start_button.addEventListener('click', function(){
-    randomize_numbers();
-    document.getElementById('start_box').style.visibility = 'hidden';
-    document.getElementById('question').style.visibility = 'visible';
-    document.getElementById('answer').style.visibility = 'visible';
-})
-
+// eventlistener of the answer, check if OK
 var submitting_answer = document.getElementById("submitting_answer");
 submitting_answer.addEventListener('keydown', function (e) {
     if (e.key === "Enter") {
@@ -60,4 +81,7 @@ submitting_answer.addEventListener('keydown', function (e) {
         submitting_answer.value = "";
     }
 })
+
+
+
 
